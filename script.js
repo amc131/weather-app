50cc753fd4d740c40942a10dd3bc52e509d48916
amc131 <76466256+amc131@users.noreply.github.com>
2023-02-16 17:51:08 -0800

    Fix formatting for Weather class construction

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // http://api.openweathermap.org/data/2.5/weather?q=London&APPID=748497c3456aa53fa1c616a6efd42483
 
+const citySearch = document.querySelector('#city-search');
+const searchBtn = document.querySelector('.search-btn');
+const cityName = 'Seattle';
 class Weather {
   constructor(name, temp, description, wind) {
     this.name = name;
@@ -9,18 +12,22 @@ class Weather {
   }
 }
 
+const buildWeather = (name, temp, description, wind) => new Weather(name, temp, description, wind);
+
 async function getAPI(url) {
   const response = await fetch(url, { mode: 'cors' });
-  const getTempData = await response.json();
-  const tempToConvert = getTempData.main.temp;
+
+  const getData = await response.json();
+  const tempToConvert = getData.main.temp;
   const convertedTemp = (1.8 * (tempToConvert - 273) + 32).toFixed(1);
-  console.log(convertedTemp);
+
+  const getWindData = getData.wind.speed;
+  const getDescription = getData.weather[0].description;
+
+  console.log(buildWeather(cityName, convertedTemp, getDescription, getWindData));
 }
 
 getAPI();
-
-const citySearch = document.querySelector('#city-search');
-const searchBtn = document.querySelector('.search-btn');
 
 searchBtn.addEventListener('click', (e) => {
   e.preventDefault();

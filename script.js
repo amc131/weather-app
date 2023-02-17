5cd195f374c7623257d4238ce37bac5a3e59082c
amc131 <76466256+amc131@users.noreply.github.com>
2023-02-16 16:04:18 -0800

    Add Weather class to populate data with

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 // http://api.openweathermap.org/data/2.5/weather?q=London&APPID=748497c3456aa53fa1c616a6efd42483
 
-async function getAPI() {
-  const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=748497c3456aa53fa1c616a6efd42483', { mode: 'cors' });
+class Weather {
+  constructor(name, temp, description, wind) {
+    this.name = name;
+    this.temp = temp;
+    this.description = description;
+    this.wind = wind;
+  }
+}
+
+async function getAPI(url) {
+  const response = await fetch(url, { mode: 'cors' });
   const getTempData = await response.json();
   const tempToConvert = getTempData.main.temp;
   console.log(tempToConvert);
 }
 
 getAPI();
+
+const citySearch = document.querySelector('#city-search');
+const searchBtn = document.querySelector('.search-btn');
+
+searchBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  getAPI(`http://api.openweathermap.org/data/2.5/weather?q=${citySearch.value}&APPID=748497c3456aa53fa1c616a6efd42483`);
+});
+
+window.onDOMContentLoaded(getAPI('http://api.openweathermap.org/data/2.5/weather?q=Seattle&APPID=748497c3456aa53fa1c616a6efd42483'));

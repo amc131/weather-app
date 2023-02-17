725f5e8ab4a28878b1cc4d30d9d5455a92843c00
amc131 <76466256+amc131@users.noreply.github.com>
2023-02-17 10:39:08 -0800

    Add function to populate weather data from object

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 // http://api.openweathermap.org/data/2.5/weather?q=London&APPID=748497c3456aa53fa1c616a6efd42483
 
-const citySearch = document.querySelector('#city-search');
-const searchBtn = document.querySelector('.search-btn');
-const cityName = 'Seattle';
+// Class to populate city data
 class Weather {
   constructor(name, temp, description, wind) {
     this.name = name;
@@ -12,7 +10,25 @@ class Weather {
   }
 }
 
+const citySearch = document.querySelector('#city-search');
+const searchBtn = document.querySelector('.search-btn');
+
+// Function to return new Weather class variable
 const buildWeather = (name, temp, description, wind) => new Weather(name, temp, description, wind);
+
+// Variable to hold the Weather object
+let cityBuild;
+
+// Function to populate data with the cityBuild variable
+const displayWeather = (object) => {
+  const cityName = document.querySelector('.city-name');
+  cityName.textContent = object.name;
+
+  const cityDescription = document.querySelector('.city-description');
+  cityDescription.textContent = object.description;
+  console.log(object);
+  citySearch.value = '';
+};
 
 async function getAPI(url) {
   const response = await fetch(url, { mode: 'cors' });
@@ -24,10 +40,9 @@ async function getAPI(url) {
   const getWindData = getData.wind.speed;
   const getDescription = getData.weather[0].description;
 
-  console.log(buildWeather(cityName, convertedTemp, getDescription, getWindData));
+  cityBuild = buildWeather(citySearch.value, convertedTemp, getDescription, getWindData);
+  displayWeather(cityBuild);
 }
-
-getAPI();
 
 searchBtn.addEventListener('click', (e) => {
   e.preventDefault();
